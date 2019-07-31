388e32d112c72540a4e3cb53ca1369654c612b1c
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-07-31 10:23:14 +0200

    Refactor required for unblocking joins implementation

--- modules/core/src/main/scala/io/scalaland/ocdquery/RepoMeta.scala
+++ modules/core/src/main/scala/io/scalaland/ocdquery/RepoMeta.scala
@@ -1,15 +1,17 @@
 package io.scalaland.ocdquery
 
-import cats.Id
+import cats.{ Functor, Id }
+import cats.syntax.functor._
 import doobie.Fragment
 import io.scalaland.ocdquery.internal._
 
 import scala.collection.immutable.{ ListMap, ListSet }
 
-trait RepoMeta[C, E, S] {
+sealed trait RepoMeta[C, E, S, N] {
   type Create = C
   type Entity = E
   type Select = S
+  type Names  = N
 
   val table:       Fragment
   val columnNames: ListSet[ColumnName]
@@ -18,7 +20,69 @@ trait RepoMeta[C, E, S] {
   val fromEntity: Entity => ListMap[ColumnName, Fragment]
   val fromSelect: Select => ListMap[ColumnName, Fragment]
 
+  def forNames[F[_]: Functor](f: Names => F[ColumnName]): F[ColumnName]
+
   lazy val * : Fragment = Fragment.const(columnNames.mkString(", "))
+}
+
+sealed trait UnnamedRepoMeta[C, E, S, N] extends RepoMeta[C, E, S, N] { repo =>
+
+  def as(name: String): NamedRepoMeta[C, E, S, N] =
+    new NamedRepoMeta[C, E, S, N] {
+
+      val table:       Fragment            = repo.table ++ Fragment.const(s" AS $name")
+      val columnNames: ListSet[ColumnName] = repo.columnNames.map(name + "." + _)
+
+      val fromCreate: Create => ListMap[ColumnName, Fragment] =
+        c => repo.fromCreate(c).map { case (k, v) => (name + "." + k) -> v }
+      val fromEntity: Entity => ListMap[ColumnName, Fragment] =
+        e => repo.fromEntity(e).map { case (k, v) => (name + "." + k) -> v }
+      val fromSelect: Select => ListMap[ColumnName, Fragment] =
+        s => repo.fromSelect(s).map { case (k, v) => (name + "." + k) -> v }
+
+      def forNames[F[_]: Functor](f: Names => F[ColumnName]): F[ColumnName] = repo.forNames(f).map("." + _)
+
+      val joinOn: Option[Fragment] = None
+    }
+}
+
+sealed trait NamedRepoMeta[C, E, S, N] extends RepoMeta[C, E, S, N] { repo1 =>
+
+  val joinOn: Option[Fragment]
+
+  def join[C1, E1, S1, N1](
+    repo2: NamedRepoMeta[C1, E1, S1, N1],
+    on:    (N => ColumnName, N1 => ColumnName)*
+  ): NamedRepoMeta[(C, C1), (E, E1), (S, S1), (N, N1)] =
+    new NamedRepoMeta[(C, C1), (E, E1), (S, S1), (N, N1)] {
+
+      // TODO: for now only join
+      val table:       Fragment            = repo1.table ++ Fragment.const(" JOIN ") ++ repo2.table
+      val columnNames: ListSet[ColumnName] = repo1.columnNames ++ repo1.columnNames
+
+      val fromCreate: ((C, C1)) => ListMap[ColumnName, Fragment] = {
+        case (c, c1) => repo1.fromCreate(c) ++ repo2.fromCreate(c1)
+      }
+      val fromEntity: ((E, E1)) => ListMap[ColumnName, Fragment] = {
+        case (e, e1) => repo1.fromEntity(e) ++ repo2.fromEntity(e1)
+      }
+      val fromSelect: ((S, S1)) => ListMap[ColumnName, Fragment] = {
+        case (s, s1) => repo1.fromSelect(s) ++ repo2.fromSelect(s1)
+      }
+
+      def forNames[F[_]: Functor](f: Names => F[ColumnName]): F[ColumnName] = repo1.forNames { n =>
+        repo2.forNames { n1 =>
+          f((n, n1))
+        }
+      }
+
+      val joinOn: Option[Fragment] = on
+        .map {
+          case (nf, n1f) =>
+            Fragment.const(repo1.forNames[Id](nf) + " = " + repo2.forNames[Id](n1f))
+        }
+        .reduceOption(_ ++ Fragment.const("AND") ++ _)
+    }
 }
 
 object RepoMeta {
@@ -31,8 +95,8 @@ object RepoMeta {
     forCreate:     FragmentsForCreate[Create, Columns],
     forEntity:     FragmentsForEntity[Entity, Columns],
     forSelect:     FragmentsForSelect[Select, Columns]
-  ): RepoMeta[Create, Entity, Select] =
-    new RepoMeta[Create, Entity, Select] {
+  ): UnnamedRepoMeta[Create, Entity, Select, Columns] =
+    new UnnamedRepoMeta[Create, Entity, Select, Columns] {
 
       val table:       Fragment            = Fragment.const(tableName)
       val columnNames: ListSet[ColumnName] = ListSet(cols.getList(columns).toSeq: _*)
@@ -43,6 +107,8 @@ object RepoMeta {
         ListMap(forEntity.toFragments(entity, columns).toSeq: _*)
       val fromSelect: Select => ListMap[ColumnName, Fragment] = select =>
         ListMap(forSelect.toFragments(select, columns).toSeq: _*)
+
+      def forNames[F[_]: Functor](f: Names => F[ColumnName]): F[ColumnName] = f(columns)
     }
 
   def forValue[ValueF[_[_]]](
@@ -53,7 +119,7 @@ object RepoMeta {
     forCreate:     FragmentsForCreate[ValueF[Id], ValueF[ColumnNameF]],
     forEntity:     FragmentsForEntity[ValueF[Id], ValueF[ColumnNameF]],
     forSelect:     FragmentsForSelect[ValueF[Selectable], ValueF[ColumnNameF]]
-  ): RepoMeta[ValueF[Id], ValueF[Id], ValueF[Selectable]] =
+  ): UnnamedRepoMeta[ValueF[Id], ValueF[Id], ValueF[Selectable], ValueF[ColumnNameF]] =
     instant[ValueF[Id], ValueF[Id], ValueF[Selectable], ValueF[ColumnNameF]](tableName, columns)
 
   def forEntity[EntityF[_[_], _[_]]](
@@ -64,7 +130,8 @@ object RepoMeta {
     forCreate:     FragmentsForCreate[EntityF[Id, UnitF], EntityF[ColumnNameF, ColumnNameF]],
     forEntity:     FragmentsForEntity[EntityF[Id, Id], EntityF[ColumnNameF, ColumnNameF]],
     forSelect:     FragmentsForSelect[EntityF[Selectable, Selectable], EntityF[ColumnNameF, ColumnNameF]]
-  ): RepoMeta[EntityF[Id, UnitF], EntityF[Id, Id], EntityF[Selectable, Selectable]] =
+  ): UnnamedRepoMeta[EntityF[Id, UnitF], EntityF[Id, Id], EntityF[Selectable, Selectable], EntityF[ColumnNameF,
+                                                                                                   ColumnNameF]] =
     instant[EntityF[Id, UnitF], EntityF[Id, Id], EntityF[Selectable, Selectable], EntityF[ColumnNameF, ColumnNameF]](
       tableName,
       columns
