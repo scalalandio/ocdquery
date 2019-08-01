c5575a5138367b25ced6353b67403c83fad461d9
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-08-01 12:19:57 +0200

    Added pagination test for fetcher, fixed bug in fetcher join, moved Sort ADT to top level

--- modules/core/src/main/scala/io/scalaland/ocdquery/Fetcher.scala
+++ modules/core/src/main/scala/io/scalaland/ocdquery/Fetcher.scala
@@ -13,18 +13,18 @@ class Fetcher[C, E: Read, S, N](val meta: NamedRepoMeta[C, E, S, N]) {
   val read: Read[E] = Read[E]
 
   def fetch(select:    Select,
-            sortOpt:   Option[(N => ColumnName, Repo.Sort)] = None,
+            sortOpt:   Option[(N => ColumnName, Sort)] = None,
             offsetOpt: Option[Long] = None,
             limitOpt:  Option[Long] = None): Query0[Entity] = {
     val orderBy = sortOpt match {
-      case Some((columnf, Repo.Sort.Ascending))  => Fragment.const(s"ORDER BY ${forNames[Id](columnf)} ASC")
-      case Some((columnf, Repo.Sort.Descending)) => Fragment.const(s"ORDER BY ${forNames[Id](columnf)} DESC")
-      case None                                  => Fragment.empty
+      case Some((columnf, Sort.Ascending))  => Fragment.const(s"ORDER BY ${forNames[Id](columnf)} ASC")
+      case Some((columnf, Sort.Descending)) => Fragment.const(s"ORDER BY ${forNames[Id](columnf)} DESC")
+      case None                             => Fragment.empty
     }
     val joinOn = joinedOn.map(fr"ON" ++ _).getOrElse(Fragment.empty)
     val where  = fr"WHERE" ++ fromSelect(select).asAnd
     val offset = offsetOpt.map(offset => Fragment.const(s"OFFSET $offset")).getOrElse(Fragment.empty)
-    val limit  = limitOpt.map(offset => Fragment.const(s"OFFSET $offset")).getOrElse(Fragment.empty)
+    val limit  = limitOpt.map(limit => Fragment.const(s"LIMIT $limit")).getOrElse(Fragment.empty)
     (fr"SELECT" ++ * ++ fr"FROM" ++ table ++ joinOn ++ where ++ orderBy ++ offset ++ limit).query[Entity]
   }
 
--- modules/core/src/test/scala/io/scalaland/ocdquery/FetcherSpec.scala
+++ modules/core/src/test/scala/io/scalaland/ocdquery/FetcherSpec.scala
@@ -25,17 +25,17 @@ class FetcherSpec extends Specification with WithH2Database {
 
   "Fetcher" should {
 
+    import TicketRepo.meta.{ Create, Entity, Names, Select }
+    type Double[A] = (A, A)
+    type Triple[A] = (A, A, A)
+
+    val join1: Fetcher[Double[Create], Double[Entity], Double[Select], Double[Names]] =
+      TicketRepo.join(TicketRepo, (TicketRepo.col(_.id), TicketRepo.col(_.id)))
+    val join2: Fetcher[Triple[Create], Triple[Entity], Triple[Select], Triple[Names]] =
+      join1.join(TicketRepo, (_._2.id, TicketRepo.col(_.id)))
+
     "generate Fragments allowing you to perform basic CRUD operations" in {
       val createTicket = Create.entity[TicketF].fromTuple(("John", "Smith", "New York", "London", LocalDate.now()))
-
-      import TicketRepo.meta.{ Create, Entity, Names, Select }
-      type Double[A] = (A, A)
-      type Triple[A] = (A, A, A)
-
-      val join1: Fetcher[Double[Create], Double[Entity], Double[Select], Double[Names]] =
-        TicketRepo.join(TicketRepo, (TicketRepo.col(_.name), TicketRepo.col(_.name)))
-      val join2: Fetcher[Triple[Create], Triple[Entity], Triple[Select], Triple[Names]] =
-        join1.join(TicketRepo, (_._2.name, TicketRepo.col(_.name)))
 
       val test: ConnectionIO[(List[Double[Entity]], List[Triple[Entity]])] = for {
         // should generate ID within SQL
@@ -67,6 +67,50 @@ class FetcherSpec extends Specification with WithH2Database {
           e2 === e3
       }
       result2 must not(empty)
+    }
+
+    "generate valid pagination" in {
+      val now = LocalDate.now()
+
+      val names = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")
+
+      val toCreate = names.map { name =>
+        Create.entity[TicketF].fromTuple((name, "TestFetcher", "TestFetcher", "TestFetcher", now))
+      }
+
+      val filter =
+        TicketRepo.emptySelect
+          .modify(_.surname)
+          .setTo("TestFetcher")
+          .modify(_.from)
+          .setTo("TestFetcher")
+          .modify(_.to)
+          .setTo("TestFetcher")
+
+      val test = for {
+        inserted <- toCreate.traverse(TicketRepo.insert(_).run).map(_.sum)
+        _ = inserted === toCreate.length
+
+        all1 <- join1.fetch((filter, filter)).to[List]
+        _ = all1.map(_._1.name).toSet === names.toSet
+        _ = all1.map(_._2.name).toSet === names.toSet
+        all2 <- join2.fetch((filter, filter, filter)).to[List]
+        _ = all2.map(_._1.name).toSet === names.toSet
+        _ = all2.map(_._2.name).toSet === names.toSet
+        _ = all2.map(_._3.name).toSet === names.toSet
+
+        firstHalf <- join2
+          .fetch((filter, filter, filter), Some(((_: Triple[Names])._1.name) -> Sort.Ascending), None, Some(5))
+          .to[List]
+        _ = firstHalf.map(_._1.name) === names.take(5)
+        _ = firstHalf.map(_._2.name) === names.take(5)
+        _ = firstHalf.map(_._3.name) === names.take(5)
+        secondHalf <- join2
+          .fetch((filter, filter, filter), Some(((_: Triple[Names])._1.name) -> Sort.Ascending), Some(5), None)
+          .to[List]
+      } yield secondHalf.map(_._1.name)
+
+      test.transact(transactor).unsafeRunSync() === names.drop(5)
     }
   }
 }
